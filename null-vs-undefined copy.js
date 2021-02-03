7985c90ccf36dce2fa6319f97978d975a2da85f5
Faerque <202001512@eastdelta.edu.bd>
2021-02-03 23:33:34 +0600

    js

--- null-vs-undefined copy.js
+++ null-vs-undefined copy.js
@@ -1,18 +1,18 @@
-//let pakhi;
+let pakhi;
 
-// function add(num1,num2){
+function add(num1,num2){
 //     console.log(num1 + num2);
-// }
+ }
 
-// function add(num1, num2){
+ function add(num1, num2){
 //     console.log(num1,num2);
-// }
+ }
 
-// const premik = {name:"smart-dude", phone:13434334};
+const premik = {name:"smart-dude", phone:13434334};
 // console.log(premik.gf); 
 
-//let fun = undefined;
+let fun = undefined;
 //console.log(fun);
 
-//let ages = [2,5,9];
+let ages = [2,5,9];
 //console.log(ages[11]);
---
+++ scope.js
@@ -0,0 +1,20 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    //console.log(result);
+    if(result > 9){
+        // const mood = "happy";
+        var mood = "happy";
+        var mood = "gappy";
+        var mood = "mappy";
+        var mood = "cappy";
+        //console.log(mood);
+    }
+    return result;
+}
+
+const output = sum(3,7);
+// console.log(bonus);
+// console.log(output);
+
